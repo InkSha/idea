4c4db8c6991bfa0c98a1fed2c2a591c889dc8ca4
InkSha <x1a010@163.com>
2024-04-05 18:46:47 +0800

    新建 todo-list 项目

--- password-manager/Form1.cs
+++
@@ -1,9 +0,0 @@
-namespace password_manager;
-
-public partial class Form1 : Form
-{
-    public Form1()
-    {
-        InitializeComponent();
-    }
-}
---
+++ todo-list/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace todo_list;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
